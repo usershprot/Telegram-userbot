5bc853932c6058f5d52ac949da4f9e9808332a02
usershprot <usershprot@gmail.com>
2026-01-07 21:40:40 +0600

    Update ubinfo.py

--- modules/ubinfo.py
+++ modules/ubinfo.py
@@ -1,11 +1,13 @@
-async def ubinfo_cmd(client, message, args):
+from telethon.tl.types import Message
+
+async def ubinfo_cmd(client, message: Message, args):
     text = (
         "Forelka Userbot\n\n"
         "Channel: @forelkauserbots\n"
         "Modules: @forelkausermodules\n"
         "Support: @forelka_support"
     )
-    await message.edit(text, disable_web_page_preview=True)
+    await message.edit(text, link_preview=False)
 
 def register(app, commands, module_name):
     commands["forelka"] = {"func": ubinfo_cmd, "module": module_name}
