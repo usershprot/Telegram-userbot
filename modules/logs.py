5f7fdef9339b2255c1aff4a6f1814cdfa0ab3ed0
usershprot <usershprot@gmail.com>
2026-01-07 21:46:40 +0600

    Update logs.py

--- modules/logs.py
+++ modules/logs.py
@@ -1,14 +1,15 @@
 import os
-from pyrogram.enums import ParseMode
+from telethon.tl.types import Message
 
-async def log_cmd(client, message, args):
+async def log_cmd(client, message: Message, args):
     log_file = "forelka.log"
     if not os.path.exists(log_file):
-        return await message.edit("<blockquote><emoji id=5778527486270770928>❌</emoji> <b>Log file not found</b></blockquote>", parse_mode=ParseMode.HTML)
+        return await message.edit("❌ Log file not found")
 
-    await message.edit("<blockquote><emoji id=5891211339170326418>⌛️</emoji> <b>Sending logs...</b></blockquote>", parse_mode=ParseMode.HTML)
-    await client.send_document("me", log_file, caption="<emoji id=5897962422169243693>👻</emoji> <b>Forelka Logs</b>")
-    await message.edit("<blockquote><emoji id=5776375003280838798>✅</emoji> <b>Logs sent to Saved Messages</b></blockquote>", parse_mode=ParseMode.HTML)
+    await message.edit("⌛️ Sending logs...")
+    await client.send_file("me", log_file, caption="👻 Forelka Logs")
+    await message.edit("✅ Logs sent to Saved Messages")
+
 
 def register(app, commands, module_name):
     commands["log"] = {"func": log_cmd, "module": module_name}
