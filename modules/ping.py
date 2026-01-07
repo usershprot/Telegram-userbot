ab5df44243de34bd715cff9ac8a1e69e196feecc
usershprot <usershprot@gmail.com>
2026-01-07 21:43:25 +0600

    Update ping.py

--- modules/ping.py
+++ modules/ping.py
@@ -1,16 +1,17 @@
 import time
-from pyrogram.enums import ParseMode
+from telethon.tl.types import Message
 
-async def ping_cmd(client, message, args):
+async def ping_cmd(client, message: Message, args):
     start = time.perf_counter()
-    await message.edit("<blockquote><emoji id=5891211339170326418>⌛️</emoji> <b>Pinging...</b></blockquote>", parse_mode=ParseMode.HTML)
-    
+    await message.edit("⌛️ Pinging...")
+
     ms = (time.perf_counter() - start) * 1000
     res = (
-        f"<emoji id=5897962422169243693>👻</emoji> <b>Pong</b>\n"
-        f"<blockquote><emoji id=5776375003280838798>✅</emoji> <b>Latency:</b> <code>{ms:.2f} ms</code></blockquote>"
+        "👻 Pong\n"
+        f"✅ Latency: {ms:.2f} ms"
     )
-    await message.edit(res, parse_mode=ParseMode.HTML)
+    await message.edit(res)
+
 
 def register(app, commands, module_name):
     commands["ping"] = {"func": ping_cmd, "module": module_name}
